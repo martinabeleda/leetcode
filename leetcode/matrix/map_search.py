ecb3e81b8f2cec9c165fd2a6196c14d89be11cdb
martinabeleda <martin.abeleda@gmail.com>
2022-11-11 14:23:25 -0800

    Add map search problem

---
+++ leetcode/matrix/map_search.py
@@ -0,0 +1,70 @@
+"""Map Search
+
+**Part A**
+
+Find the adjacent positions you can move to given a location on the map below:
+
+
+    1 = wall (cannot move to this position)
+    0 = not wall (can move to this position)
+    
+    For example, an input of (2,3) means that you will be determining the possible moves at 
+    location “row 2” and “column 3”. The index of the input starts at 0
+
+
+**Part B**
+
+Determine if you can reach the destination from a given location on the map.
+
+"""
+from typing import List, Tuple, Set
+from copy import deepcopy
+
+
+def possible_moves(map: List[List[int]], location: Tuple[int, int]) -> Set[Tuple[int, int]]:
+    """Find the adjacent positions you can move to given a location on the map"""
+    y, x = location
+    moves = [
+        (y - 1, x),
+        (y + 1, x),
+        (y, x - 1),
+        (y, x + 1),
+    ]
+    result = set()
+    for move in moves:
+        next_y = move[0]
+        next_x = move[1]
+
+        # Check the boundary conditions
+        if (next_y < 0) or (next_x < 0):
+            continue
+        if (next_y >= len(map)) or (next_x >= len(map[0])):
+            continue
+
+        if map[next_y][next_x] == 0:
+            result.add(move)
+
+    return result
+
+
+def is_reachable(map: List[List[int]], current_location: Tuple[int, int], destination: Tuple[int, int]) -> bool:
+    """Determine if you can reach the destination from a given location
+
+    Approach:
+        - depth first search.
+        - search over all possible moves until we can't anymore.
+        - check if the destination is in those
+        - we use the same map to store visitation
+    """
+    visited = deepcopy(map)
+    return _is_reachable(visited, current_location, destination)
+
+
+def _is_reachable(map: List[List[int]], current_location: Tuple[int, int], destination: Tuple[int, int]) -> bool:
+    next_locations = possible_moves(map, current_location)
+    for next_location in next_locations:
+        if next_location == destination:
+            return True
+
+    map[current_location[0]][current_location[1]] = 1
+    return any(is_reachable(map, l, destination) for l in next_locations)
---
+++ leetcode/matrix/test/test_map_search.py
@@ -0,0 +1,49 @@
+from typing import List
+
+import pytest
+
+from leetcode.matrix.map_search import possible_moves, is_reachable
+
+
+@pytest.fixture
+def map_a() -> List[List[int]]:
+    return [
+        [0, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 1],
+        [0, 0, 0, 1, 0],
+    ]
+
+
+@pytest.mark.parametrize(
+    "location,expected",
+    [
+        ((2, 2), {(1, 2), (2, 1), (2, 3), (3, 2)}),
+        ((0, 0), {(1, 0), (0, 1)}),
+        ((2, 0), {(1, 0), (2, 1)}),
+        ((5, 4), set()),
+    ],
+)
+def test_possible_moves(location, expected, map_a):
+    assert possible_moves(map_a, location) == expected
+
+
+@pytest.fixture
+def map_bc() -> List[List[int]]:
+    return [[0, 0, 0, 1, 0], [0, 1, 0, 1, 0], [0, 0, 0, 0, 0], [1, 1, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0]]
+
+
+@pytest.mark.parametrize(
+    "location,destination,expected",
+    [
+        ((0, 0), (2, 0), True),
+        ((2, 1), (0, 4), True),
+        ((2, 1), (4, 1), False),
+        ((4, 0), (0, 2), False),
+        ((0, 4), (0, 2), True),
+    ],
+)
+def test_possible_moves(location, destination, expected, map_bc):
+    assert is_reachable(map_bc, location, destination) == expected
